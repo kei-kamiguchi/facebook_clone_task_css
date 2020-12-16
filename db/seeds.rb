58b33d59b87be85be2646da175b7a37f237acabd
keikamiguchi <kei.kamiguchi517@gmail.com>
2020-12-16 12:05:28 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.create(name:"山田 太郎", email:"sample@gmail.com", password: "password", password_confirmation: "password", profile_image:File.open("./public/images/スクリーンショット 2020-12-16 11.26.03.png"), background_image:File.open("./public/images/スクリーンショット 2020-12-16 11.29.42.png"))
+
+10.times do
+  Post.create(content: "React は、Facebookとコミュニティによって開発されているユーザインタフェース構築のためのJavaScriptライブラリである。React.jsまたはReactJSの名称でも知られている。 Reactはシングルページアプリケーションやモバイルアプリケーションの開発におけるベースとして使用することができる。", image:File.open("./public/images/0A65B68C-25B0-42B8-BA58-449E2717B36E_1_105_c.jpeg"), user_id: 1)
+end
